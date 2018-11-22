99754662ed2f8147fdcae17184012b7bdcb54516
cperales <cperales@uloyola.es>
2018-11-22 13:48:50 +0100

    Tests with unittest (3)

---
+++ tests.py
@@ -0,0 +1,6 @@
+import unittest
+from test.test_full import TestFullExample
+from test.test_cluster import TestCluster
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/__main__.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from test_full import TestFullExample
-from test_cluster import TestCluster
-
-if __name__ == '__main__':
-    unittest.main()
