d4efdc956a04c82ad8d2d618ca25ec0d0d9f8a40
cperales <cperales@uloyola.es>
2018-11-27 11:16:55 +0100

    setup.py with more information

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
 from setuptools import setup
 
 setup(
-    name="foucluster",
-    description="Clustering of songs using Fourier",
-    version="1.2",
-    author="Carlos Perales",
-    author_email="sir.perales@gmail.com",
+    name='foucluster',
+    description='Clustering of songs using Fourier Transform',
+    long_description='Similarities among songs are computed using Fast Fourier'
+                'Transform. With this information, unsupervised machine learning'
+                 ' is applied.',
+    url='https://github.com/cperales/foucluster',
+    version='1.2',
+    author='Carlos Perales',
+    author_email='sir.perales@gmail.com',
+    keywords=['cluster', 'Fourier', 'music', 'song',
+              'machine learning', 'kmeans'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: Artistic Software',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis'
+    ],
     packages=['foucluster'
               ],
     zip_safe=False,
